c92744e9d5b60e9927c03a223526264505641214
SorrenJ <soalexjao@gmail.com>
2024-07-11 14:31:59 -0700

    component imports on app, import useStates

--- fancy-buttons/src/App.js
+++ fancy-buttons/src/App.js
@@ -1,6 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import AngryButton from './components/AngryButton';
+import CounterButton from './components/CounterButton';
+import LightSwitchButton from './components/LightSwitchButton';
+import TextRepeaterButton from './components/TextRepeaterButton';
 function App() {
   return (
     <div className="App">
