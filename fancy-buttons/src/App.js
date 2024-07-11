d6463a3b6ed9f1c1a41af9a61d0e5e7babed2755
SorrenJ <soalexjao@gmail.com>
2024-07-11 12:57:42 -0700

    create components, hardcoded values

---
+++ fancy-buttons/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Fancy Buttons!</h1>
+      <section>
+        <AngryButton />
+        <CounterButton />
+        <LightSwitchButton />
+        <TextRepeaterButton />
+      </section>
+    </div>
+  );
+}
+
+export default App;
