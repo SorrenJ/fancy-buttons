2eecf736d5bf928d45e3c96fa45fbd0243205225
SorrenJ <soalexjao@gmail.com>
2024-07-11 15:53:42 -0700

    intialize useStates

--- fancy-buttons/src/components/LightSwitchButton.js
+++ fancy-buttons/src/components/LightSwitchButton.js
@@ -1,10 +1,16 @@
-import React from 'react'; //optional
+import { useState } from "react";
 
 function LightSwitchButton(){
+  const [light, setLight] = useState('off');
   return (
+    
     <button className="LightSwitchButton">
-      {/* When the state is on */}<span className="on"><i>💡</i> I'm on!</span>
-      {/* When the state is off */}<span className="off"><i>💡</i> I'm off!</span>
+      {/* When the state is on */}
+ 
+
+      {light === 'on' && <span className="on"><i>💡</i> I'm on!</span>}
+      {/* When the state is off */}
+      {light === 'off' && <span className="off"><i>💡</i> I'm off!</span>}
     </button>
   );
 }
--- fancy-buttons/src/components/TextRepeaterButton.js
+++ fancy-buttons/src/components/TextRepeaterButton.js
@@ -1,9 +1,15 @@
 import { useState } from "react";
 
 function TextRepeaterButton(){
-  return (
+    const [repetitions, setRepetitions] = useState(1);
+  let textArray = [];
+
+  for ( let i = 0; i < repetitions; i++){
+    textArray.push(<span key={i}>I like this text</span>)
+  }
+    return (
     <button className="TextRepeaterButton">
-      <span>I like this text</span>
+    {textArray}
     </button>
   );
 }
